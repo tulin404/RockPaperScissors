4a327c2f65e657aa0937a538f04c3f74e66a5da9
Tulio <221194504+tulin404@users.noreply.github.com>
2025-09-04 21:24:24 -0300

    Finished it.

--- script.js
+++ script.js
@@ -4,9 +4,46 @@ const images = [
     "img/scissors.png"
 ]
 
+const choices = [
+    "Rock",
+    "Paper",
+    "Scissors"
+]
+
+// BUTTONS
+// Add the event listeners
+document.getElementById("previous-btn").addEventListener("click", previousImg)
+document.getElementById("next-btn").addEventListener("click", nextImg)
+document.getElementById("play-btn").addEventListener("click", playGame)
+let currentIndex = 0
+
+// Changes the choices and images based on the index of the array
+function updateChoice (index) {
+    document.getElementById("choice").innerText = choices[index]
+}
+
+function showImg (index) {
+    document.getElementById("img").src = images[index]
+};
+
+// Changes the index
+function previousImg () {
+    currentIndex = (currentIndex - 1 + images.length) % images.length
+    showImg(currentIndex)
+    updateChoice(currentIndex)
+};
+
+function nextImg () {
+    currentIndex = (currentIndex + 1) % images.length
+    showImg(currentIndex)
+    updateChoice(currentIndex)
+}
+
+// CHOICES
+// Gets the computer choice
 function getComputerChoice() {
     // Gets a number (0, 1 or 2)
-    let computerChoice = () => Math.floor(Math.random() * 3)
+    let computerChoice =  Math.floor(Math.random() * 3)
     // Change the random number to the following strings
     if (computerChoice == 0) {
         return ("rock")
@@ -20,7 +57,6 @@ function getComputerChoice() {
 function getHumanChoice() {
     // Gets the user's choice
     let humanChoice = document.getElementById("choice").innerText
-    console.log(humanChoice)
     // Adjust and returns the user choices
     if (humanChoice.toLowerCase() === "rock") {
         return ("rock")
@@ -33,54 +69,52 @@ function getHumanChoice() {
     }
 };
 
+// GAME
+// Global variables
+let humanScore = 0
+let computerScore = 0
 
+// Function that executes the game
 function playGame() {
-    // Score variables
-    let humanScore = 0
-    let computerScore = 0
-
     // Function that makes the rules and decides the winner of the round
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice) {
-            return (console.log("That's a tie!"))
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            alert("That's a tie!")
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ) {
+            alert(`You win! ${humanChoice} beats ${computerChoice}`)
             humanScore++
-            return (console.log("You win! Rock beats Scissors"))
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            computerScore++
-            return (console.log("You lose! Paper beats Rock"))
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore++
-            return (console.log("You win! Paper beats Rock"))
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            computerScore++
-            return (console.log("You lose! Scissors beats Paper"))
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore++
-            return (console.log("You win! Scissors beats Paper"))
-        } else if (humanChoice === "scissors" && computerChoice === "rock"){
-            computerScore++
-            return (console.log("You lose! Rock beats Scissors"))
         } else {
-            return undefined
+            alert(`You lose! ${computerChoice} beats ${humanChoice}`)
+            computerScore++
         }
     };
+    
+    // Action time, these lines define the arguments that will be used and call the function using it
+    let humanChoice = getHumanChoice()
+    let computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+    // These lines update the DOM, making the score at the page update
+    document.getElementById("human-score").innerText = humanScore
+    document.getElementById("computer-score").innerText = computerScore
 
-    // Loop to play 5 rounds
-    for (i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice()
-        const computerSelection = getComputerChoice()
-        playRound(humanSelection, computerSelection)
+    // We playing 5 rounds and these lines define the winner
+    if ((humanScore + computerScore) == 5) {
+        if (computerScore > humanScore) {
+            alert(`The computer won from ${computerScore} to ${humanScore}`)
+        } else if (humanScore > computerScore) {
+            alert(`You win from ${humanScore} to ${computerScore}`)
+        } else {
+            alert(`It's a tie game! ${humanScore} to ${computerScore}`)
+        }
+    
+    // Reset game for next match
+    humanScore = 0
+    computerScore = 0
+    document.getElementById("human-score").innerText = humanScore
+    document.getElementById("computer-score").innerText = computerScore
     }
-
-    // Final result
-    if (humanScore > computerScore) {
-        return (console.log("Human wins!"))
-    } else if (computerScore > humanScore) {
-        return (console.log("Computer wins!"))
-    } else {
-        return (console.log("That's a tie."))
-    }
-};
-
-playGame()
+}
